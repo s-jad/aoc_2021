a7cca9f93749f0e42ff1a14977e1b5d8aa2149d3
s-jad <s_jad@web.de>
2023-12-17 07:25:54 +0100

    Feat: part 2 day 1 completed

---
+++ day_1/src/bin/part_2.rs
@@ -0,0 +1,36 @@
+use itertools::Itertools;
+use std::time::Instant;
+
+fn process(input: &str) -> usize {
+    input
+        .lines()
+        .map(|n| n.parse::<usize>().expect("Is number"))
+        .tuple_windows::<(_, _, _)>()
+        .map(|(a, b, c)| a + b + c)
+        .tuple_windows()
+        .filter(|(a, b)| a < b)
+        .count()
+}
+
+fn main() {
+    let input = include_str!("../../input.txt");
+
+    let start = Instant::now();
+    let output = process(input);
+    let time = start.elapsed();
+
+    dbg!(output, time);
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let input = include_str!("../../test.txt");
+        let output = process(input);
+        assert_eq!(result,);
+    }
+}
