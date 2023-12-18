2068e84c4330498dc67b8e44222579e17b22a186
s-jad <s_jad@web.de>
2023-12-18 14:14:35 +0100

    Feat: optimized with arr instead of vec 9.7ms -> 4.3ms

--- day_9/src/bin/part_1.rs
+++ day_9/src/bin/part_1.rs
@@ -1,20 +1,18 @@
 use itertools::Itertools;
 use std::time::Instant;
 
-fn get_neighbour(grid: &Vec<Vec<usize>>, x: usize, y: usize) -> Vec<Option<usize>> {
-    let dx = vec![-1, 0, 1, 0];
-    let dy = vec![0, 1, 0, -1];
+fn get_neighbour(grid: &Vec<Vec<usize>>, x: usize, y: usize) -> [Option<usize>; 4] {
+    let dx = [-1, 0, 1, 0];
+    let dy = [0, 1, 0, -1];
 
-    let mut neighbours = Vec::new();
+    let mut neighbours = [Option::None; 4];
 
     for i in 0..4 {
         let nx = x as i32 + dx[i];
         let ny = y as i32 + dy[i];
 
         if nx >= 0 && ny >= 0 && ny < grid[0].len() as i32 && nx < grid.len() as i32 {
-            neighbours.push(Some(grid[nx as usize][ny as usize]));
-        } else {
-            neighbours.push(None);
+            neighbours[i] = Some(grid[nx as usize][ny as usize]);
         }
     }
 
@@ -43,12 +41,12 @@ fn process(input: &str) -> usize {
             }
 
             if local_min {
-                local_minimums.push((grid[x][y], x, y));
+                local_minimums.push(grid[x][y]);
             }
         }
     }
 
-    local_minimums.iter().map(|(i, _, _)| i + 1).sum()
+    local_minimums.iter().map(|i| i + 1).sum()
 }
 
 fn main() {
