af86a4f8e517999a632711614053c4e9ddd9c546
s-jad <s_jad@web.de>
2023-12-18 10:13:25 +0100

    Feat: solved part 2 day 7

---
+++ day_7/src/bin/part_2.rs
@@ -0,0 +1,53 @@
+use itertools::Itertools;
+use std::time::Instant;
+
+fn process(input: &str) -> usize {
+    let mut crab_pos = input
+        .split_terminator(&[',', '\n'][..])
+        .map(|s| s.parse::<usize>().expect("num"))
+        .collect_vec();
+
+    let crab_len = crab_pos.len();
+    let mean = (crab_pos.iter().sum::<usize>() as f64 / crab_len as f64).ceil() as usize;
+
+    let mut final_total: f64 = 999999999f64;
+
+    for i in -1..=1 {
+        let total = crab_pos
+            .iter()
+            .map(move |c| {
+                ((*c as isize - (mean as isize + i) as isize).abs() as f64 / 2f64)
+                    * (1f64 + (*c as isize - (mean as isize + i) as isize).abs() as f64)
+            })
+            .sum::<f64>();
+
+        if total <= final_total {
+            final_total = total;
+        }
+    }
+
+    final_total as usize
+}
+
+fn main() {
+    let input = include_str!("../../input.txt");
+
+    let start = Instant::now();
+    let output = process(input);
+    let time = start.elapsed();
+
+    dbg!(output, time);
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let input = include_str!("../../test.txt");
+        let output = process(input);
+        assert_eq!(result,);
+    }
+}
